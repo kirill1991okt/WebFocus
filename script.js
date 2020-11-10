eb344a582719145eb31dca8cf96c978fd9bde2aa
kirill1991okt <kirill1991okt@gmail.com>
2020-11-10 21:27:25 +0300

    feat: realise courusel

---
+++ script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 1,
+    nav: true,
+    navigationText: ['<img src="./assets/icon/chevron-left.png" class="main__direction-btn main__direction-prev">','<img src="./assets/icon/chevron-right.png"  class="main__direction-btn main__direction-next">'],
+    dots: false
+})
+
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     items: 2,
+//     nav: true,
+//     navigationText: ['<img src="./assets/icon/chevron-left.png" class="main__direction-btn main__direction-prev">','<img src="./assets/icon/chevron-right.png"  class="main__direction-btn main__direction-next">'],
+//     dots: true,
+//     margin: 30
+// })
+
