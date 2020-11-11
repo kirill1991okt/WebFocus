5a61b1d728df12fcbc95211b507376b1e4a2602d
kirill1991okt <kirill1991okt@gmail.com>
2020-11-11 16:54:41 +0300

    feat: review

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $('.owl-carousel').owlCarousel({
     items: 1,
     nav: true,
     navigationText: ['<img src="./assets/icon/chevron-left.png" class="main__direction-btn main__direction-prev">','<img src="./assets/icon/chevron-right.png"  class="main__direction-btn main__direction-next">'],
-    dots: false
+    dots: false,
 })
 
 // $('.owl-carousel').owlCarousel({
